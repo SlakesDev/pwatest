ba367fdceb73fdbe0ccb49632433fc17c4190a92
JemilCodes <slakesdev@gmail.com>
2024-03-07 12:39:47 +0100

    deploy2

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -3,6 +3,7 @@ import Todos from "../component/Todos";
 import { cookieBasedClient } from "../utils/amplify-utils";
 
 import { onDeleteTodo } from "../_actions/actions";
+import Link from "next/link";
 
 export default async function Home() {
   let { data: todos } = await cookieBasedClient.models.Todo.list({
@@ -14,7 +15,16 @@ export default async function Home() {
 
   return (
     <div className=" w-full h-screen flex justify-center items-center">
-      <Todos todos={todos} onDeleteTodo={onDeleteTodo} />
+      {todos.length > 0 ? (
+        <Todos todos={todos} onDeleteTodo={onDeleteTodo} />
+      ) : (
+        <div>
+          <p className=" text-text text-lg"> You have no todos </p>
+          <Link className="text-text text-sm" href={"/create"}>
+            Create Todo
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
